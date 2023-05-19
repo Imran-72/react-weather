a79f84ff06e3ac04f8a3dafe9a42efddd04d03a0
iabdurakhmanov <iabdurakhmanov@iterion.ru>
2023-05-19 11:01:55 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+import path from 'path';
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+  },
+};
